ce788a7525a634dfc452e7e9910dcbe9373a0161
ajitdikle <Ajitdikle@1020>
2023-01-03 16:06:11 +0530

    Add project files.

---
+++ EmployeeManagment/LoginUITest.cs
@@ -0,0 +1,25 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace EmployeeManagment
+{
+    public class LoginUITest
+    {
+
+        
+
+        [Test]
+        public void ValidateTitleTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+            string actualTitle = driver.Title;
+            Assert.That(actualTitle,Is.EqualTo("OrangeHRM"));
+
+
+        }
+    }
+}
