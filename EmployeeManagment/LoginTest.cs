037450e7aa6bd13c352b20dc255ca4e5e5bb1bd7
ajitdikle <Ajitdikle@1020>
2023-01-03 18:16:16 +0530

    updated

---
+++ EmployeeManagment/LoginTest.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagment.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment
+{
+    public class LoginTest : AutomationWrapper
+
+    {
+        [Test]
+        public void ValidLoginTest()
+        {
+            driver.FindElement(By.Name("username")).SendKeys("Admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin123");
+            driver.FindElement(By.XPath("//button[@type = 'submit']")).Click();
+
+
+
+        }
+        
+
+
+    }
+}
--- EmployeeManagment/LoginUITest.cs
+++ EmployeeManagment/LoginUITest.cs
@@ -1,31 +1,12 @@
+using EmployeeManagment.Base;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace EmployeeManagment
 {
-    public class LoginUITest
+    public class LoginUITest : AutomationWrapper
     {
-        IWebDriver driver;
-        [SetUp]
-
-        public void BeforeMethod()
-        {
-
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-            driver.Url = "https://opensource-demo.orangehrmlive.com/";
-
-        }
-        [TearDown]
-        public void AfterMethod() 
-        {
         
-        driver.Quit();
-        
-        
-        }
-
         
 
         
