6edbd2a7c96bea31661f145418a848861bc64ff5
ajitdikle <Ajitdikle@1020>
2023-01-03 16:43:03 +0530

    update

--- EmployeeManagment/LoginUITest.cs
+++ EmployeeManagment/LoginUITest.cs
@@ -5,21 +5,51 @@ namespace EmployeeManagment
 {
     public class LoginUITest
     {
+        IWebDriver driver;
+        [SetUp]
+
+        public void BeforeMethod()
+        {
+
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+
+        }
+        [TearDown]
+        public void AfterMethod() 
+        {
+        
+        driver.Quit();
+        
+        
+        }
+
+        
 
         
 
         [Test]
         public void ValidateTitleTest()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+           
 
-            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+            
             string actualTitle = driver.Title;
             Assert.That(actualTitle,Is.EqualTo("OrangeHRM"));
 
 
+        }
+        [Test]
+        public void ValidatePlaceholderTest()
+        {
+           
+
+           string actualUsernamePlaceholder= driver.FindElement(By.Name("username")).GetAttribute("placeholder");
+            Assert.That(actualUsernamePlaceholder, Is.EqualTo("Username"));
+            string actualPasswordPlaceholder = driver.FindElement(By.Name("password")).GetAttribute("placeholder");
+            Assert.That(actualPasswordPlaceholder, Is.EqualTo("Password"));
         }
     }
 }
