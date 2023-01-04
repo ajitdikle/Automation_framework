3e641bbb6e5f768c5fb906caf4e4f6e0bb567825
ajitdikle <Ajitdikle@1020>
2023-01-04 17:04:12 +0530

    update of testcase

--- EmployeeManagment/LoginTest.cs
+++ EmployeeManagment/LoginTest.cs
@@ -18,7 +18,56 @@ namespace EmployeeManagment
             driver.FindElement(By.Name("password")).SendKeys("admin123");
             driver.FindElement(By.XPath("//button[@type = 'submit']")).Click();
 
+            string actualUrl = driver.Url;
+            Assert.That(actualUrl, Is.EqualTo("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"));
 
+
+
+
+        }
+
+        public static object[] InvalidLoginData()
+        {
+
+
+            string[] dataset1 = new string[3];
+            dataset1[0] = "john";
+            dataset1[1] = "john123";
+            dataset1[2] = "invalid credentials";
+
+            string[] dataset2 = new string[2];
+            dataset2[0] = "peter";
+            dataset2[1] = "peter123";
+            dataset1[2] = "invalid credentials";
+            string[] dataset3 = new string[2];
+            dataset3[0] = "saul";
+            dataset3[1] = "saul123";
+            dataset1[2] = "invalid credentials";
+
+            object[] allDataSet = new object[3];
+            allDataSet[0] = dataset1;
+            allDataSet[1] = dataset2;
+            allDataSet[2] = dataset3;
+
+            return allDataSet;
+
+
+        }
+        [Test, TestCaseSource(nameof(InvalidLoginData))]
+
+        //[Test]
+        //[TestCase("john","john123", "Invalid credential")]
+        //[TestCase("peter","peter123","Invalid credential")]
+        public void InvalidLoginTest(string username, string password, string expectedError)
+        {
+
+            driver.FindElement(By.Name("username")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.XPath("//button[@type = 'submit']")).Click();
+
+           string actualError= driver.FindElement(By.XPath("//p[contains(normalize-space(),'cred')]")).Text;
+
+            Assert.That(actualError.Contains(expectedError));
 
         }
         
