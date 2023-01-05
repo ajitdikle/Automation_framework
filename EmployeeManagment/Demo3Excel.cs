39700d8d3c611a8e7554f1898d3926f1d4905d5e
ajitdikle <Ajitdikle@1020>
2023-01-05 17:05:21 +0530

    Excel

---
+++ EmployeeManagment/Demo3Excel.cs
@@ -0,0 +1,28 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment
+{
+   public class Demo3Excel
+    {
+        [Test]
+        public void DemoExcelRead()
+        { 
+        
+        XLWorkbook book = new XLWorkbook(@"C:\Users\ajitd\Desktop\CSharp\AutomationFramework\EmployeeManagment\TestData\Orange_data.xlsx");
+
+            IXLWorksheet sheet = book.Worksheet("InvalidLoginData");
+
+            IXLRange range = sheet.RangeUsed();
+
+            string value = range.Cell(2,2).GetString();
+            Console.WriteLine(value);
+        
+        }
+
+    }
+}
